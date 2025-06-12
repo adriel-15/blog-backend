6d85e2cce22beb1ef83f8d3064cfe16d4c0a1196
adriel <adriel15rosario@gmail.com>
2025-06-11 20:15:49 -0400

    db update
    udpate init.sql to delete the poc table

--- init.sql
+++ init.sql
@@ -1,7 +1,9 @@
 use `arblog-db`;
 
-CREATE TABLE `authority` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `auth` varchar(128) NOT NULL,
-  PRIMARY KEY (`id`)
-) AUTO_INCREMENT=1;
+--CREATE TABLE `authority` (
+--  `id` int NOT NULL AUTO_INCREMENT,
+--  `auth` varchar(128) NOT NULL,
+--  PRIMARY KEY (`id`)
+--) AUTO_INCREMENT=1;
+
+delete table if exists `authority`;
