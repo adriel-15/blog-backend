5ebfdf281f020d4ed9b4b3c84382bc84a033396e
adriel <adriel15rosario@gmail.com>
2025-06-11 20:18:26 -0400

    db
    fix inti.sql script

--- init.sql
+++ init.sql
@@ -1,9 +1,3 @@
 use `arblog-db`;
 
---CREATE TABLE `authority` (
---  `id` int NOT NULL AUTO_INCREMENT,
---  `auth` varchar(128) NOT NULL,
---  PRIMARY KEY (`id`)
---) AUTO_INCREMENT=1;
-
 delete table if exists `authority`;
