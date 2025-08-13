49e3af8b52445a087156580d967f3e9ea2d490c8
adriel <adriel15rosario@gmail.com>
2025-08-13 11:05:46 -0400

    refactor emailServiceImpl

--- src/main/java/com/arprojects/blog/infrastructure/aop/services/EmailServiceImpl.java
+++ src/main/java/com/arprojects/blog/infrastructure/aop/services/EmailServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"prod"})
+@Profile("prod")
 public class EmailServiceImpl implements EmailService{
 
     private final JavaMailSender javaMailSender;
